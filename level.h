b12f4422920968062bd096aebcf27b0624bd6dcd
Hdvpdrm <hardcoderyan@gmail.com>
2024-12-22 18:41:07 +0300

    added some comments

--- level.h
+++ level.h
@@ -6,10 +6,10 @@
 #include<ctype.h>
 #include<string.h>
 
-bool is_numeric(char* str);
+bool is_numeric(char* str); //check whether passed string contains only digital characters
 
-bool parse_dim_val(int* val, char* line);
-void replaces_fillers(char* line);
+bool parse_dim_val(int* val, char* line); //get width and height of level
+void replaces_fillers(char* line); //replace + with spaces
 bool read_level(char** output, char* filename, int* _width, int* _height);
 
 #endif //LEVEL_H
