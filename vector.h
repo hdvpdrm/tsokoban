c84a86244be20e29d87696c1265d9014fce11e7c
Hdvpdrm <hardcoderyan@gmail.com>
2025-01-09 01:04:13 +0300

    added macro to check whether two vectors are equal

--- vector.h
+++ vector.h
@@ -10,6 +10,8 @@ typedef struct
 Vector2i vector2d_create(int x, int y);
 Vector2i vector2d_add(Vector2i vec1, Vector2i vec2);
 
+#define VEQ(a,b) (a.x == b.x && a.y == b.y)
+
 #define RIGHT    (Vector2i){0, 1}
 #define LEFT  (Vector2i){0, -1}
 #define UP  (Vector2i){-1, 0}
