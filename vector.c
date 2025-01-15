7a545367b558669089e07eb4d815942780f919a9
Hdvpdrm <hardcoderyan@gmail.com>
2025-01-15 23:52:03 +0300

    added function to init vector with value of other one. Also added function to substract vectors

--- vector.c
+++ vector.c
@@ -1,3 +1,4 @@
+
 #include"vector.h"
 
 Vector2i vector2d_create(int x, int y)
@@ -14,4 +15,20 @@ Vector2i vector2d_add(Vector2i vec1, Vector2i vec2)
     result.x = vec1.x + vec2.x;
     result.y = vec1.y + vec2.y;
     return result;
+}
+Vector2i vector2d_sub(Vector2i vec1, Vector2i vec2)
+{
+    Vector2i result;
+    result.x = vec1.x - vec2.x;
+    result.y = vec1.y - vec2.y;
+    return result;
+}
+void vector2d_fill(Vector2i* dest, Vector2i* source)
+{
+  if(dest == NULL ||
+     source == NULL) return;
+
+  dest->x = source->x;
+  dest->y = source->y;
+  
 }
--- vector.h
+++ vector.h
@@ -1,5 +1,6 @@
 #ifndef VECTOR_H
 #define VECTOR_H
+#include<stddef.h>
 
 typedef struct
 {
@@ -9,12 +10,14 @@ typedef struct
 
 Vector2i vector2d_create(int x, int y);
 Vector2i vector2d_add(Vector2i vec1, Vector2i vec2);
+Vector2i vector2d_sub(Vector2i vec1, Vector2i vec2);
+void vector2d_fill(Vector2i* dest, Vector2i* source);
 
 #define VEQ(a,b) (a.x == b.x && a.y == b.y)
 
 #define RIGHT    (Vector2i){0, 1}
-#define LEFT  (Vector2i){0, -1}
-#define UP  (Vector2i){-1, 0}
-#define DOWN (Vector2i){1, 0}
+#define LEFT     (Vector2i){0, -1}
+#define UP       (Vector2i){-1, 0}
+#define DOWN     (Vector2i){1, 0}
 
 #endif
